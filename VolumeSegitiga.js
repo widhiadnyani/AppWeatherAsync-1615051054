2d01b028d3c00d0c9cc390aff0982bf3959e73a2
widhiadnyani <37166909+widhiadnyani@users.noreply.github.com>
2018-03-26 20:38:18 +0800

    Add files via upload

---
+++ VolumeSegitiga.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Button,
+  Text, TextInput,
+  View
+} from 'react-native';
+
+export default class Hitungvolumebalok extends Component {
+  
+}
+AppRegistry.registerComponent('AppForm2', () => Hitungvolumebalok);
